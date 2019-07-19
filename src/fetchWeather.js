41c00edf01e725670822d3ad48d5f43dfe2c846c
dmytro vitenko <cubagw84@gmail.com>
2019-07-19 19:59:37 +0300

    fixed fetch

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -1,6 +1,6 @@
 export default {
   fetchOfWeather(city) {
-    const baseUrl = 'http://api.apixu.com/v1/current.json';
+    const baseUrl = 'https://api.apixu.com/v1/current.json';
     const myKey = '?key=3106a8f4d1914e2f921142834191507';
     const targetCity = `&q=${city}`;
 
